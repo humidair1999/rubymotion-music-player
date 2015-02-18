0d38576d2e7ee1e46248b7af852f10e7764a1c2d
J. Ky Marsh <jkymarsh@gmail.com>
2015-02-17 17:11:29 -0800

    begin playing with styling windows

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -5,11 +5,28 @@ class AppDelegate
   end
 
   def buildWindow
-    @mainWindow = NSWindow.alloc.initWithContentRect([[240, 180], [480, 360]],
-      styleMask: NSTitledWindowMask|NSClosableWindowMask|NSMiniaturizableWindowMask|NSResizableWindowMask,
+    @mainWindow = NSWindow.alloc.initWithContentRect(
+      [[240, 180], [480, 360]],
+      styleMask: NSClosableWindowMask|NSMiniaturizableWindowMask|NSResizableWindowMask,
       backing: NSBackingStoreBuffered,
-      defer: false)
-    @mainWindow.title = NSBundle.mainBundle.infoDictionary['CFBundleName']
-    @mainWindow.orderFrontRegardless
+      defer: false
+    ).tap do |window|
+      window.title = NSBundle.mainBundle.infoDictionary['CFBundleName']
+      # window.setAlphaValue(0.9)
+      window.setBackgroundColor(NSColor.colorWithCalibratedRed(52.0/255.0, green: 152.0/255.0, blue: 219.0/255.0, alpha: 255.0/255.0))
+      # window.setOpaque(false)
+      window.orderFrontRegardless
+
+      closeButton = NSButton.alloc.initWithFrame(
+        [[10, 10], [100, 22]]
+      ).tap do |button|
+        button.title = 'Close'
+        button.bezelStyle = NSTexturedRoundedBezelStyle
+        button.target = window
+        button.action = 'close'
+      end
+
+      window.contentView.addSubview(closeButton)
+    end
   end
 end
