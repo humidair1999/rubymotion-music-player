1d8a43cfdf27953b32c3d7beb5502e9991e15e43
J. Ky Marsh <jkymarsh@gmail.com>
2015-02-20 02:43:14 -0800

    remove unused transport controller

--- app/controllers/transport_controls_controller.rb
+++
@@ -1,8 +0,0 @@
-# TODO: probably should be an instance of NSViewController?
-class TransportControlsController
-    include AppHelper
-
-    def initialize(opts)
-        p opts[:scrollView]
-    end
-end
